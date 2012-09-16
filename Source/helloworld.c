72758cd11e34cefe9217d4345acbad6ad3202409
Luis Ibanez <luis.ibanez@kitware.com>
2012-09-16 17:03:34 -0400

    Improved following gtm_access example

--- Source/helloworld.c
+++ Source/helloworld.c
@@ -1,27 +1,62 @@
+/*=========================================================================
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
 #include "gtmxc_types.h"
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+// GT.M limits
+#define maxcode 8192     // maximum length of a line of code for the compiler / variable name
+#define maxmsg  2048     // maximum length of a GT.M message
+#define maxstr  1048576  // maximum length of a value
+
+// GT.M call wrapper - if an error in call or untrappable error in GT.M, print error on STDERR, clean up and exit
+#define CALLGTM(xyz) status = xyz ;		\
+  if (0 != status ) {				\
+    gtm_zstatus( msg, maxmsg );			\
+    fprintf( stderr, "%s\n", msg );		\
+    gtm_exit();					\
+    return status ;				\
+  }
+
 
 int main()
 {
-  /* Initialize */
-  gtm_status_t gtm_init(void);
 
-  gtm_long_t buf_len = 2050;
-  gtm_char_t * msg_buffer = (gtm_char_t*) gtm_malloc(buf_len);
+  gtm_char_t err[maxmsg];
+  gtm_char_t msg[maxmsg];
+  gtm_char_t value[maxstr];
+  gtm_char_t var[maxcode];
 
-  /* Get status */
-  gtm_zstatus(msg_buffer, buf_len);
+  gtm_status_t status;
+  gtm_string_t p_value;
 
-  printf("status = %s\n",msg_buffer);
+  // Initialization
+  p_value.address = (xc_char_t *) &value;
+  p_value.length = 0;
 
-  /* Shutdown */
-  gtm_free(msg_buffer);
 
-  gtm_status_t status_on_exit = gtm_exit ();
+  // Initialize GT.M runtime
+  CALLGTM( gtm_init() );
 
-  printf("final status = %d\n",status_on_exit);
+
+  // Cleanup GT.M runtime
+  CALLGTM( gtm_exit( ));
 
   return 0;
 }
